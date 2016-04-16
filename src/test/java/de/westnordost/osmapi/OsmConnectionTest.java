80c94b509dee9e2e8a59c965349cfb01cb4a0a48
Tobias Zwick <newton@openclonk.org>
2016-04-16 21:44:23 +0200

    add test for OsmConnectionTest

--- src/test/java/de/westnordost/osmapi/OsmConnectionTest.java
+++ src/test/java/de/westnordost/osmapi/OsmConnectionTest.java
@@ -1,6 +1,52 @@
 package de.westnordost.osmapi;
 
-public class OsmConnectionTest
+import java.io.InputStream;
+
+import junit.framework.TestCase;
+import de.westnordost.osmapi.errors.OsmApiReadResponseException;
+import de.westnordost.osmapi.errors.OsmAuthorizationException;
+import de.westnordost.osmapi.errors.OsmConnectionException;
+
+public class OsmConnectionTest extends TestCase
 {
-	// TODO...
+	public void testAuthorizationException()
+	{
+		try
+		{
+			OsmConnection osm = ConnectionTestFactory.createConnection(null);
+			osm.makeAuthenticatedRequest("doesntMatter", "GET");
+			fail();
+		}
+		catch(OsmAuthorizationException e) {}
+	}
+	
+	public void testConnectionException()
+	{
+		try
+		{
+			OsmConnection osm = new OsmConnection("http://cant.connect.to.this.server.hm", "blub", null);
+			osm.makeRequest("doesntMatter", null);
+			fail();
+		}
+		catch(OsmConnectionException e) {}
+	}
+	
+	public void testErrorParsingApiResponse()
+	{
+		try
+		{
+			OsmConnection osm = ConnectionTestFactory.createConnection(null);
+			osm.makeRequest("capabilities", new ApiResponseReader<Void>()
+			{
+				@Override
+				public Void parse(InputStream in) throws Exception
+				{
+					throw new Exception();
+				}
+			});
+			fail();
+		}
+		catch(OsmApiReadResponseException e) {}
+	}
+
 }
