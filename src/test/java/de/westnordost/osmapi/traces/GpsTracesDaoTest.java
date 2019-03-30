d4007d241627a93b6e78947a39631aaf6135f360
Tobias Zwick <newton@openclonk.org>
2019-03-30 15:35:37 +0100

    enable tests for GpsTracesDao

--- src/test/java/de/westnordost/osmapi/traces/GpsTracesDaoTest.java
+++ src/test/java/de/westnordost/osmapi/traces/GpsTracesDaoTest.java
@@ -1,13 +1,16 @@
 package de.westnordost.osmapi.traces;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import de.westnordost.osmapi.ConnectionTestFactory;
 import de.westnordost.osmapi.common.Handler;
 import de.westnordost.osmapi.common.errors.OsmAuthorizationException;
 import de.westnordost.osmapi.common.errors.OsmNotFoundException;
+import de.westnordost.osmapi.map.data.OsmLatLon;
 import de.westnordost.osmapi.traces.GpsTraceDetails.Visibility;
 import junit.framework.TestCase;
 
@@ -19,11 +22,10 @@ public class GpsTracesDaoTest extends TestCase
 	private static final int TEN_MINUTES = 1000 * 60 * 10;
 	
 	private static final int NONEXISTING_TRACE = 0;
-// TODO blocked by https://github.com/openstreetmap/chef/pull/31
-//	private static final int PRIVATE_TRACE_OF_OTHER_USER = 0; // TODO upload test traces!
-//	private static final int TRACE_OF_UNPRIVILEGED_USER = 0;// TODO upload test traces!
-//	private static final int TRACE_OF_PRIVILEGED_USER = 0;// TODO upload test traces!
-	
+
+	private static final int PRIVATE_TRACE_OF_OTHER_USER = 928;
+	private static final int PUBLIC_TRACE = 927;
+
 	
 	private GpsTracesDao privilegedDao;
 	private GpsTracesDao unprivilegedDao;
@@ -37,36 +39,36 @@ public class GpsTracesDaoTest extends TestCase
 				ConnectionTestFactory.User.ALLOW_NOTHING));
 	}
 	
-//	public void testAccessPrivateTraceOfOtherUserResultsInFailure()
-//	{
-//		try
-//		{
-//			privilegedDao.get(PRIVATE_TRACE_OF_OTHER_USER);
-//			fail();
-//		}
-//		catch(OsmAuthorizationException e) { }
-//		
-//		try
-//		{
-//			privilegedDao.delete(PRIVATE_TRACE_OF_OTHER_USER);
-//			fail();
-//		}
-//		catch(OsmAuthorizationException e) { }
-//		
-//		try
-//		{
-//			privilegedDao.getData(PRIVATE_TRACE_OF_OTHER_USER, null);
-//			fail();
-//		}
-//		catch(OsmAuthorizationException e) { }
-//		
-//		try
-//		{
-//			privilegedDao.update(PRIVATE_TRACE_OF_OTHER_USER, Visibility.PUBLIC, null, null);
-//			fail();
-//		}
-//		catch(OsmAuthorizationException e) { }
-//	}
+	public void testAccessPrivateTraceOfOtherUserResultsInFailure()
+	{
+		try
+		{
+			privilegedDao.get(PRIVATE_TRACE_OF_OTHER_USER);
+			fail();
+		}
+		catch(OsmAuthorizationException ignore) { }
+
+		try
+		{
+			privilegedDao.delete(PRIVATE_TRACE_OF_OTHER_USER);
+			fail();
+		}
+		catch(OsmAuthorizationException ignore) { }
+
+		try
+		{
+			privilegedDao.getData(PRIVATE_TRACE_OF_OTHER_USER, null);
+			fail();
+		}
+		catch(OsmAuthorizationException ignore) { }
+
+		try
+		{
+			privilegedDao.update(PRIVATE_TRACE_OF_OTHER_USER, Visibility.PUBLIC, "test", null);
+			fail();
+		}
+		catch(OsmAuthorizationException ignore) { }
+	}
 	
 	public void testAccessTraceWithoutPrivilegesResultsInFailure()
 	{
@@ -77,33 +79,33 @@ public class GpsTracesDaoTest extends TestCase
 		}
 		catch(OsmAuthorizationException ignore) { }
 		
-//		try
-//		{
-//			unprivilegedDao.get(TRACE_OF_UNPRIVILEGED_USER);
-//			fail();
-//		}
-//		catch(OsmAuthorizationException e) { }
-//		
-//		try
-//		{
-//			unprivilegedDao.delete(TRACE_OF_UNPRIVILEGED_USER);
-//			fail();
-//		}
-//		catch(OsmAuthorizationException e) { }
-//		
-//		try
-//		{
-//			unprivilegedDao.getData(TRACE_OF_UNPRIVILEGED_USER, null);
-//			fail();
-//		}
-//		catch(OsmAuthorizationException e) { }
-//		
-//		try
-//		{
-//			unprivilegedDao.update(TRACE_OF_UNPRIVILEGED_USER, Visibility.PUBLIC, null, null);
-//			fail();
-//		}
-//		catch(OsmAuthorizationException e) { }
+		try
+		{
+			unprivilegedDao.get(PUBLIC_TRACE);
+			fail();
+		}
+		catch(OsmAuthorizationException ignore) { }
+
+		try
+		{
+			unprivilegedDao.delete(PUBLIC_TRACE);
+			fail();
+		}
+		catch(OsmAuthorizationException ignore) { }
+
+		try
+		{
+			unprivilegedDao.getData(PUBLIC_TRACE, null);
+			fail();
+		}
+		catch(OsmAuthorizationException ignore) { }
+
+		try
+		{
+			unprivilegedDao.update(PUBLIC_TRACE, Visibility.PUBLIC, "test", null);
+			fail();
+		}
+		catch(OsmAuthorizationException ignore) { }
 		
 		try
 		{
@@ -138,12 +140,12 @@ public class GpsTracesDaoTest extends TestCase
 	
 	public void testTooLongDescription()
 	{
-//		try
-//		{
-//			privilegedDao.update(TRACE_OF_PRIVILEGED_USER, Visibility.TRACKABLE, tooLong(), null);
-//			fail();
-//		}
-//		catch(IllegalArgumentException e) { }
+		try
+		{
+			privilegedDao.update(PUBLIC_TRACE, Visibility.TRACKABLE, tooLong(), null);
+			fail();
+		}
+		catch(IllegalArgumentException ignore) { }
 		
 		try
 		{
@@ -156,7 +158,7 @@ public class GpsTracesDaoTest extends TestCase
 	
 	public void testTooLongTags()
 	{
-		List<String> tags = new ArrayList<String>();
+		List<String> tags = new ArrayList<>();
 		tags.add("abc");
 		tags.add(tooLong());
 		
@@ -168,48 +170,48 @@ public class GpsTracesDaoTest extends TestCase
 		}
 		catch(IllegalArgumentException ignore) { }
 		
-//		try
-//		{
-//			privilegedDao.update(TRACE_OF_PRIVILEGED_USER, Visibility.TRACKABLE, null, tags);
-//			fail();
-//		}
-//		catch(IllegalArgumentException e) { }
+		try
+		{
+			privilegedDao.update(PUBLIC_TRACE, Visibility.TRACKABLE, "test", tags);
+			fail();
+		}
+		catch(IllegalArgumentException ignore) { }
 	}
 	
-//	public void testCreateGetUpdateDelete()
-//	{
-//		List<String> tags = new ArrayList<>();
-//		tags.add("a tag");
-//		List<GpsTrackpoint> points = new ArrayList<>();
-//		points.add(new GpsTrackpoint(new OsmLatLon(1.23,3.45)));
-//		
-//		long id = privilegedDao.create("test case", Visibility.PRIVATE, "test case desc", tags,
-//				points);
-//		
-//		GpsTraceDetails trace = privilegedDao.get(id);
-//		assertEquals("test case", trace.name);
-//		assertEquals(Visibility.PRIVATE, trace.visibility);
-//		assertEquals("test case desc", trace.description);
-//		assertEquals("a tag", trace.tags.get(0));
-//		assertEquals("osmagent-test-allow-everything", trace.userName);
-//		assertTrue(Math.abs(new Date().getTime() - trace.date.getTime()) < TEN_MINUTES);
-//		
-//		privilegedDao.update(id, Visibility.TRACKABLE, null, null);
-//		trace = privilegedDao.get(id);
-//		assertEquals(Visibility.TRACKABLE, trace.visibility);
-//		assertEquals(null, trace.description);
-//		assertEquals(null, trace.tags);
-//		
-//		privilegedDao.delete(id);
-//		trace = privilegedDao.get(id);
-//		assertNull(trace);
-//	}
+	public void testCreateGetUpdateDelete()
+	{
+		List<String> tags = new ArrayList<>();
+		tags.add("a tag, another");
+		List<GpsTrackpoint> points = new ArrayList<>();
+		points.add(new GpsTrackpoint(new OsmLatLon(1.23,3.45)));
+
+		long id = privilegedDao.create("test case", Visibility.PRIVATE, "test case desc", tags,
+				points);
+
+		GpsTraceDetails trace = privilegedDao.get(id);
+		assertEquals("test_case", trace.name);
+		assertEquals(Visibility.PRIVATE, trace.visibility);
+		assertEquals("test case desc", trace.description);
+		assertEquals(Arrays.asList("a tag","another"), trace.tags);
+		assertEquals("osmagent-test-allow-everything", trace.userName);
+		assertTrue(Math.abs(new Date().getTime() - trace.date.getTime()) < TEN_MINUTES);
+
+		privilegedDao.update(id, Visibility.TRACKABLE, "desc", null);
+		trace = privilegedDao.get(id);
+		assertEquals(Visibility.TRACKABLE, trace.visibility);
+		assertEquals("desc",trace.description);
+		assertNull(trace.tags);
+
+		privilegedDao.delete(id);
+		trace = privilegedDao.get(id);
+		assertNull(trace);
+	}
 	
 	private static String tooLong()
 	{
-		String result = "";
-		for(int i = 0; i<=256; ++i) result += "x";
-		return result;
+		StringBuilder result = new StringBuilder();
+		for(int i = 0; i<=256; ++i) result.append("x");
+		return result.toString();
 	}
 	
 }
