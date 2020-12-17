5e4b8ff13cd82e1734f1e8775fd1638516cf6a24
Tobias Zwick <newton@westnordost.de>
2020-12-17 16:35:37 +0100

    new version
    pay heed to that SimpleDateFormat is not thread-safe, thus, having a static SimpleDateFormat is a bad idea.

--- libs/core/src/main/java/de/westnordost/osmapi/common/OsmXmlDateFormat.java
+++ libs/core/src/main/java/de/westnordost/osmapi/common/OsmXmlDateFormat.java
@@ -8,18 +8,21 @@ import java.util.TimeZone;
 /** Format used to represent dates within the OSM Api 0.6 (except notes)*/
 public class OsmXmlDateFormat
 {
-	private static final SimpleDateFormat DEFAULT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-	static {
-		DEFAULT.setTimeZone(TimeZone.getTimeZone("UTC"));
+	private final SimpleDateFormat dateFormat;
+
+	public OsmXmlDateFormat()
+	{
+		dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+		dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 	}
 	
 	public Date parse(String source) throws ParseException
 	{
-		return DEFAULT.parse(source);
+		return dateFormat.parse(source);
 	}
 	
 	public String format(Date date)
 	{
-		return DEFAULT.format(date);
+		return dateFormat.format(date);
 	}
 }
