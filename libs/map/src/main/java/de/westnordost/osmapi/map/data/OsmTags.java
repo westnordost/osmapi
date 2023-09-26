3f069529300f97d19c5f33ec8ef71acf0f61f52b
Tobias Zwick <newton@westnordost.de>
2023-09-26 15:06:49 +0200

    remove checking the string length of osm keys and values
    turns out that the method used to count the utf code points length (.codePoints().count()) is not available as part of their desugaring process, i.e. not available for API levels below 24. Since this library's main user is StreetComplete (min API 21), I will just remove this check.

--- libs/map/src/main/java/de/westnordost/osmapi/map/data/OsmTags.java
+++ libs/map/src/main/java/de/westnordost/osmapi/map/data/OsmTags.java
@@ -3,8 +3,7 @@ package de.westnordost.osmapi.map.data;
 import java.io.Serializable;
 import java.util.Map;
 
-/** Represent the tags for any osm element. It is a Map of Strings which registers whether or not it
- *  has been modified and whose keys and values are both limited to less than 256 characters */
+/** Represent the tags for any osm element. It is a Map of Strings which registers whether it has been modified  */
 public class OsmTags extends ModificationAwareMap<String, String> implements Serializable
 {
 	private static final long serialVersionUID = 1L;
@@ -12,40 +11,5 @@ public class OsmTags extends ModificationAwareMap<String, String> implements Ser
 	public OsmTags(Map<String, String> map)
 	{	
 		super(map);
-		for(Entry<String, String> entry : map.entrySet())
-		{
-			checkKeyValueLength(entry.getKey(), entry.getValue());
-		}
-	}
-
-	@Override
-	public String put(String key, String value)
-	{
-		checkKeyValueLength(key, value);
-		return super.put(key, value);
-	}
-
-	@Override
-	public void putAll(Map<? extends String, ? extends String> map)
-	{
-		for(Entry<? extends String, ? extends String> entry : map.entrySet())
-		{
-			checkKeyValueLength(entry.getKey(), entry.getValue());
-		}
-		super.putAll(map);
-	}
-
-	private static void checkKeyValueLength(String key, String value)
-	{
-		if(key.codePoints().count() >= 256)
-		{
-			throw new IllegalArgumentException("For key \"" + key + "\": Key length is limited" +
-					" to less than 256 characters.");
-		}
-		if(value.codePoints().count() >= 256)
-		{
-			throw new IllegalArgumentException("For value \"" + value + "\": Value length is " +
-					"limited to less than 256 characters.");
-		}
 	}
 }
